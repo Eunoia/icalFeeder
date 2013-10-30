0b6766f1c3bd13e74618849723085d93ca6fffdf
Evan R <eunoia.github@gmail.com>
2013-10-30 14:05:32 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IcalFeeder::Application.config.secret_key_base = '193bedc121c50de9b4cbc2d26c124e3c4fb0cd7b61cb9633532bbfb863c91275d9a7d5c81905fa0787602701b5d5d71a1a8a2ee01ada1f7ccb3f0752e23cf512'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IcalFeeder::Application.config.session_store :cookie_store, key: '_icalFeeder_session'
